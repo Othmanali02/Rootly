2ea60ddb6817901439e6fe6d23a5f3fda76af838
Othmanali02 <othman90hijawi@gmail.com>
2025-02-25 14:51:39 -0500

    menu burger (in progress)

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,7 +1,21 @@
 <script setp>
 import "../assets/app.css";
 
+
 export default {
+  data() {
+    return {
+      menuStatus: false
+    };
+  },
+  methods: {
+    handleMenuOpen() {
+      this.menuStatus = true;
+    },
+    handleMenuClose() {
+      this.menuStatus = false;
+    }
+  },
   name: 'NavbarComponent'
 };
 </script>
@@ -10,10 +24,13 @@ export default {
   <nav class="w-full py-6 shadow-md">
     <div class="navItems text-center px-4">
       <a href="/">
-        <img src="../assets/logomockup6.png" alt="Rootly" class="h-24 text-center mx-auto" /> 
+        <img src="../assets/logomockup6.png" alt="Rootly" class="h-24 text-center mx-auto" />
       </a>
-      
+
       <div class="flex space-x-3 justify-center items-center mt-4 md:mt-0">
+        <a @click="handleMenuOpen" class="text-black cursor-pointer hover:text-gray-400 text-lg">
+          <img class="h-8" src="../assets/menuBurger.png" alt=" " />
+        </a>
         <a href="/info" class="text-black hover:text-gray-400 text-lg">
           <img class="h-8" src="../assets/info.png" alt=" " />
         </a>
@@ -22,13 +39,31 @@ export default {
         </a>
       </div>
     </div>
-    <hr>  
- </nav>
+    <hr>
+  </nav>
 
+  <div v-if="menuStatus" class="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
+    <div class="bg-white p-6  h-[450px]  overflow-y-auto rounded-lg shadow-xl w-1/3">
+      <div class="flex items-center justify-between">
+        <h2 class="text-xl font-semibold text-gray-800 mb-4">
+          Quick Access
+        </h2>
+        <button class="px-4 py-2 font-bold bg-red-500 text-white rounded-lg hover:bg-red-600" @click="handleMenuClose">
+          Close
+        </button>
+      </div>
+
+      <ul>
+        <li><a href="/dashboard">Dashboard</a></li>
+        <li><a href="/dashboard">Dashboard</a></li>
+        <li><a href="/dashboard">Dashboard</a></li>
+
+      </ul>
+    </div>
+  </div>
 </template>
 
 
 
 
-<style scoped>
-</style>
+<style scoped></style>
